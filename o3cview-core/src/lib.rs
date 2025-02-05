abf217dab698df1933c8b188c4f528057a7d13a1
Khang <khang06@users.noreply.github.com>
2025-02-05 13:32:00 -0500

    Better bad USB connection handling

--- o3cview-core/src/lib.rs
+++ o3cview-core/src/lib.rs
@@ -39,10 +39,15 @@ impl Viewer {
     }
 
     fn open_device(&mut self) -> bool {
-        self.hid.refresh_devices().unwrap();
+        if self.hid.reset_devices().is_err() || self.hid.add_devices(SAYO_VENDOR_ID, 0).is_err() {
+            return false;
+        }
         for x in self.hid.device_list() {
             if x.vendor_id() == SAYO_VENDOR_ID && x.usage_page() == API_V2_FAST_USAGE_PAGE {
                 if let Ok(device) = x.open_device(&self.hid) {
+                    if device.set_blocking_mode(false).is_err() {
+                        return false;
+                    }
                     self.device = Some(device);
                     return true;
                 }
@@ -58,7 +63,8 @@ impl Viewer {
     ) -> Result<(), HidError> {
         let device = self.device.as_mut().unwrap();
 
-        for i in (0..fb.len()).step_by(1024 - 0xC) {
+        const CHUNK_SIZE: usize = 1024 - 0xC;
+        for i in (0..fb.len()).step_by(CHUNK_SIZE) {
             // Clear checksum
             self.req[2] = 0;
             self.req[3] = 0;
@@ -78,11 +84,14 @@ impl Viewer {
         }
 
         // Get response
-        'outer: for x in fb.chunks_mut(1024 - 0xC) {
+        'outer: for (i, x) in fb.chunks_mut(CHUNK_SIZE).enumerate() {
             for _ in 0..5 {
-                device.read_timeout(&mut self.res, 250)?;
+                if device.read_timeout(&mut self.res, 4)? == 0 {
+                    // Probably a dropped packet somewhere
+                    return Ok(());
+                }
 
-                if self.res[0] == API_V2_FAST_REPORT_ID {
+                if self.res[0] == API_V2_FAST_REPORT_ID && u32::from_le_bytes(self.res[8..12].try_into().unwrap()) as usize == i * CHUNK_SIZE {
                     // Copy to framebuffer
                     x.copy_from_slice(&self.res[12..(12 + x.len())]);
                     continue 'outer;
